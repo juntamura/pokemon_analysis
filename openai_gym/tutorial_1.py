6a93f3220c4655fb8a915dcc600ab2f488de6cb8
owdowt <satoshiowada1020@gmail.com>
2018-12-07 19:56:10 +0900

    add openaigym tutorial_2

--- openai_gym/tutorial_1.py
+++ openai_gym/tutorial_1.py
@@ -3,9 +3,9 @@ Reference: https://gym.openai.com/docs/
 """
 
 import gym
+
 env = gym.make('CartPole-v0')
 env.reset()
 for _ in range(1000):
     env.render()
-    env.step(env.action_space.sample()) # take a random action
-
+    env.step(env.action_space.sample())  # take a random action
